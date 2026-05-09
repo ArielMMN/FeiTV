f4b884040a5b9caadd57fab6494d3f34e6b40fd0
ArielMMN <arielmmoraes@gmail.com>
2026-05-09 16:55:43 -0300

    Criação das classes e seus atributos, com seus gets and setters

---
+++ src/model/Filme.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ariel
+ */
+public class Filme extends Video {
+    private int duracao; // em minutos
+
+    public Filme(int idVideo, String titulo, String descricao, int anoLancamento, int duracao) {
+        super(idVideo, titulo, descricao, "filme", anoLancamento);
+        this.duracao = duracao;
+    }
+
+    public int getDuracao() { return duracao; }
+    public void setDuracao(int duracao) { this.duracao = duracao; }
+}
